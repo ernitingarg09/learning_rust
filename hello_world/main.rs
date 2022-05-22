d1ee2880b580a7e54c361fda2e9da2adc27970c9
Nitin Garg <nitin.garg@bitflyer.com>
2022-05-22 22:52:49 +0900

    Learning rust

---
+++ hello_world/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    /*
+       # formating tool "rustfmt"
+       # check version "rustc --version"
+       # compile "rustc main.rs"
+       # run "./main.exe" (on linux) ".\main.exe" (on windows)
+       # dynamic language: python, javascript & ruby
+    */
+
+    println!("hello world");
+}
